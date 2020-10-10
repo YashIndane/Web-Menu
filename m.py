208f7dac9e404ba6ff83c71dc7eee5e44fd21717
Yash Indane <yashindane46@gmail.com>
2020-10-10 17:56:33 +0530

    Update m.py

--- m.py
+++ m.py
@@ -11,23 +11,35 @@ cmd = form_values.getvalue("v")
 
 cmd = cmd.lower()
 
-if 'free ram' in cmd : 
+if 'free -m' in cmd or 'free ram' in cmd : 
     process_output = spb.getoutput('free -m')
 
 elif 'date' in cmd : 
     process_output = spb.getoutput('date')
 
-elif 'calendar' in cmd : 
+elif 'cal' in cmd or 'calendar' in cmd : 
     process_output = spb.getoutput('cal')
 
-elif 'ip configuration' : 
-    process_output = spb.getoutput('sudo ifconfig enp0s3')
-
-elif 'cpu details' in cmd : 
+elif 'lscpu' in cmd  or 'cpu details' in cmd : 
     process_output = spb.getoutput('sudo lscpu')
 
-elif 'drop cash' in cmd :
-    process_output = spb.getoutput('sudo echo 3 > /proc/sys/vm/drop_caches')
+#elif 'drop cash' in cmd :
+#    process_output = spb.getoutput('sudo echo 3 > /proc/sys/vm/drop_caches')
+
+elif 'df -h' in cmd or 'memory information' in cmd : 
+     process_output = spb.getoutput('sudo df -h')
+
+elif 'systemctl status httpd' in cmd or 'server status' in cmd:
+     process_output = spb.getoutput('sudo systemctl status httpd')
+
+elif 'ps -aux' in cmd or 'running processes' in cmd : 
+     process_output = spb.getoutput('sudo ps -aux')
+
+elif 'netstat -tnlp' in cmd  or 'ports' in cmd : 
+     process_output = spb.getoutput('sudo netstat -tnlp')
+
+elif 'ifconfig enp0s3' in cmd or 'ip configuration' in cmd :
+    process_output = spb.getoutput('sudo ifconfig enp0s3')
 
 print(process_output)
 
