e8fee53f465d299354b27f65e5473604fd98629b
Yash Indane <yashindane46@gmail.com>
2020-10-17 09:29:35 +0530

    Update dk.js

--- dk.js
+++ dk.js
@@ -1,20 +1,27 @@
 const button = document.getElementById('sb');
-//const space = document.getElementById('sp');
-//const textfield = document.getElementById('t');
-//const submitbutton = document.getElementById('sub');
+const osname = document.getElementById('d_')
+const version = document.getElementById('v_');
+const na = document.getElementById('n_');
+const submitbutton = document.getElementById('ss');
 
-//const SR = window.SpeechRecognition || window.webkitSpeechRecognition;
-//const recog = new SR();
+const SR = window.SpeechRecognition || window.webkitSpeechRecognition;
+const recog = new SR();
+var counter = 0;
+const fields = [osname , version , na];
 
-//recog.onstart = () => console.log('speak');
+recog.onstart = () => console.log('speak');
 
-//recog.onresult = function(event){
-//                 const current = event.resultIndex;
-//                 const transcript = event.results[current][0].transcript;
-//                 textfield.value = transcript;
-//                 submitbutton.click();
-//};
-//button.onclick = () => recog.start();
+recog.onresult = function(event){
+                 const current = event.resultIndex;
+                 const transcript = event.results[current][0].transcript;
+                 
+                 fields[counter].value = transcript;
+
+                 
+                 //textfield.value = transcript;
+                 //submitbutton.click();
+};
+
 
 function speak(msg){
         
@@ -26,4 +33,22 @@ function speak(msg){
        window.speechSynthesis.speak(sph);
 }
 
-button.onclick = () => speak("b");
+
+
+function getDetails(){
+
+       speak("Please tell your OS");
+       recog.start();
+       counter += 1;
+       speak("Please tell your version");
+       recog.start();
+       counter += 1;
+       speak("What name you would like to give to the OS?");
+       recog.start();
+       speak("Launching the OS")
+       submitbutton.click();
+
+}
+
+button.onclick = () => getDetails();
+
