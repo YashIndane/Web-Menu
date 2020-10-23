810dd8e6ea956ec0057aef5231ab858a3776ff90
Yash Indane <yashindane46@gmail.com>
2020-10-23 10:46:29 +0530

    Add files via upload

---
+++ Python/drun.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+
+from subprocess import getstatusoutput as gso
+import cgi
+
+print('content-type:text/plain')
+print()
+
+form_values = cgi.FieldStorage()
+
+cmd = form_values.getvalue('dcmd').lower()
+osname = form_values.getvalue('osname')
+osimage = form_values.getvalue('img')
+
+
+if 'systemctl start docker' in cmd or 'start docker' in cmd: 
+
+  s = gso('sudo systemctl start docker')
+  print('Docker started sucessfully!' if s[0]==0 else 'Failed to start docker!')
+
+elif 'systemctl stop docker' in cmd or 'stop docker' in cmd:
+ 
+   s = gso('sudo systemctl stop docker')
+   print('Docker stopped sucessfully' if s[0]==0 else 'Failed to stop docker!')
+
+elif 'docker run' in cmd :
+  
+  s = gso(f'sudo docker run -d -i -t --name {osname} {osimage}')
+  print('OS launched sucessfully!' if s[0] == 0 else 'Failed to launch!')
+
+elif 'docker stop' in cmd:
+
+   s = gso('sudo ' + cmd)
+   print('OS stopped sucessfully!' if s[0] == 0 else 'Failed to stop!')
+
+elif 'docker ps' in cmd or 'docker status' in cmd :
+   
+   print(gso('sudo docker ps')[1])
+
+elif 'docker pull' in cmd : 
+   
+   os = cmd.split(' ')[-1]
+   s = gso('sudo ' + 'docker pull ' + os)
+   print(os + 'downloaded sucessfully!' if s[0] == 0 else 'Failed to download!')
+
+elif 'docker images' in cmd or 'available images' in cmd: 
+ 
+   print(f'''--------------DOCKER-IMAGES---------------\n
+         {gso('sudo docker images')[1]}''')
+
+elif 'systemctl status docker' in cmd : 
+ 
+   print(f'''--------------DOCKER-STATUS---------------\n
+         {gso('sudo ' + cmd)[1]}''')
+
+else : print('NO SUCH COMMAND!')
---
+++ Python/hadoop_congf.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+import subprocess as spb
+
+IP = input("Enter the IP of remote system -> ")
+conf = input("Configuration type -> ")
+
+if conf.lower() == 'datanode' : 
+
+    hadoop = 'hadoop-1.2.1-1.x86_64.rpm'
+    jdk = 'jdk-8u171-linux-x64.rpm'
+   
+    operations = [
+                  f'scp /root/{jdk} {IP}:/root' ,
+                  f'scp /root/{hadoop} {IP}:/root',
+                  f'ssh {IP} rpm -i {jdk}' ,
+                  f'ssh {IP} rpm -i {hadoop} --force' 
+                 ]
+
+    for op in operations : 
+          
+           status = spb.getstatusoutput(op)
