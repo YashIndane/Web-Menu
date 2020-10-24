15c5508052261afe07a998d3f09b50359c4a9399
Yash Indane <yashindane46@gmail.com>
2020-10-24 09:25:21 +0530

    Add files via upload

---
+++ Python/hadoop_conf.py
@@ -0,0 +1,145 @@
+#!/usr/bin/python3
+import subprocess as spb
+
+
+
+
+def buildNode(mode , IP , PORT , IP2 = 'none', file_='none' , filem_ = 'none') : 
+
+    hadoop = 'hadoop-1.2.1-1.x86_64.rpm'
+    jdk = 'jdk-8u171-linux-x64.rpm'
+   
+    installations = [
+                     f'scp /root/{jdk} {IP}:/root' ,
+                     f'scp /root/{hadoop} {IP}:/root',
+                     f'ssh {IP} rpm -i {jdk}' ,
+                     f'ssh {IP} rpm -i {hadoop} --force'
+                    ]
+
+    for op in installations : status = spb.getstatusoutput(op)
+    
+    #Building the file
+    if mode == 'datanode' : 
+
+        filestatus = spb.getstatusoutput(f'ssh {IP} mkdir /{file_}')
+      
+    #Creating core-site file
+    #core-site file configuration is same for client,master,slave
+    ip = IP2 if mode in ('datanode' , 'client') else IP
+
+    core_ins = [
+                '\<configuration\> ',
+
+                '',
+
+                '\<property\> ',
+
+                '\<name\>fs.default.name\</name\> ',
+
+                f'\<value\>hdfs://{ip}:{PORT}\</value\> ',
+
+                '\</property\> ',
+
+                '',
+
+                '\</configuration\> '
+               ]
+
+    cs = spb.getstatusoutput('cp /root/core-site.xml /var/www/cgi-bin')
+
+    for ins in core_ins : 
+           cs = spb.getstatusoutput('echo ' + ins + '>> core-site.xml')
+
+    
+    #creating hdfs-site file
+    if mode in ('datanode' , 'namenode') : 
+
+       X = 'data' if mode == 'datanode' else 'name'
+       F =  file_ if mode == 'datanode' else filem_ 
+
+       hdfs_ins = [
+                   '\<configuration\> ',
+
+                   '',
+
+                   '\<property\> ',
+
+                   f'\<name\>dfs.{X}.dir\</name\> ',
+
+                   f'\<value\>/{F}\</value\> ',
+
+                   '\</property\> ',
+
+                   '',
+
+                   '\</configuration\> '
+                  ]
+       cs = spb.getstatusoutput('cp /root/hdfs-site.xml /var/www/cgi-bin')
+     
+       for ins_ in hdfs_ins : 
+          
+            cs = spb.getstatusoutput('echo ' + ins_ + '>> hdfs-site.xml')
+       
+       sc = spb.getstatusoutput(f'ssh {IP} rm /etc/hadoop/hdfs-site.xml')
+       sc = spb.getstatusoutput(f'scp hdfs-site.xml {IP}:/etc/hadoop')
+       sc = spb.getstatusoutput('rm hdfs-site.xml')
+      
+
+    #deleating and sending new files
+
+    sc = spb.getstatusoutput(f'ssh {IP} rm /etc/hadoop/core-site.xml')
+    sc = spb.getstatusoutput(f'scp core-site.xml {IP}:/etc/hadoop')
+    sc = spb.getstatusoutput(f'rm core-site.xml')
+  
+     
+       
+        
+    if mode == 'namenode' : 
+           
+           filestatus = spb.getstatusoutput(f'ssh {IP} mkdir /{filem_}')
+           filestatus = spb.getstatusoutput(f'ssh {IP} hadoop namenode -format')
+    
+    if mode in ('namenode' , 'datanode') : 
+
+       runService = input('''
+                          ---------------------------------------------
+                          Do you want to start the service?[Y/N]
+                          ---------------------------------------------
+                          '''
+                         )
+    
+       if runService == 'Y' :
+   
+            sc = spb.getstatusoutput(f'ssh {IP} hadoop-daemon.sh start {mode}')
+            print(f'{mode} launched!' if sc[0] == 0 else 'Failed to launch!')
+            sc = spb.getstatusoutput('ssh {IP} jps')
+
+    else : print(f'{mode} launched!')       
+   
+#############################################################################
+
+IP = input("Enter the IP of remote system -> ")
+conf = input("Configuration type -> ")
+
+if conf.lower() == 'datanode' : 
+
+    ip_master = input('Enter ip of master -> ')
+    file_name = input('Enter your file name -> ')
+    port = input('Enter service port number -> ')
+    buildNode('datanode' , IP , port , ip_master , file_name , 'none')
+
+elif conf.lower() == 'namenode':
+    
+    master_file_name = input('Enter your file name -> ')
+    port = input('Enter service port number -> ')
+    buildNode('namenode' , IP , port , 'none' , 'none' , master_file_name)    
+ 
+elif conf.lower() == 'client':
+    
+    ip_master = input('Enter ip of master -> ')
+    port = input('Enter ip of service port -> ')
+    buildNode('client' , IP , port , ip_master , 'none' , 'none')   
+
+               
+
+   
