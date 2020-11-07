1fb689f413753ca6b8d1fdb2511582cac4ffe717
Yash Indane <yashindane46@gmail.com>
2020-11-07 10:02:51 +0530

    Update m.py

--- Python/m.py
+++ Python/m.py
@@ -23,17 +23,25 @@ elif 'cal' in cmd or 'calendar' in cmd :
 elif 'lscpu' in cmd  or 'cpu details' in cmd : 
     process_output = spb.getoutput('sudo lscpu')
 
+elif 'systemctl status' in cmd or 'status of' in cmd :
+    x = cmd.split(' ')[-1]
+    process_output = spb.getoutput('sudo systemctl status ' + x)
+
+elif 'systemctl start' in cmd or 'start' in cmd : 
+    y = cmd.split(' ')[-1]
+    process_output = spb.getstatusoutput('sudo systemctl start ' + y)
+    print(y + ' started sucessfully!' if process_output[0] == 0 else 'Failed!')
+
+elif 'systemctl stop' in cmd or 'stop' in cmd : 
+    z = cmd.split(' ')[-1]
+    process_output = spb.getstatusoutput('sudo systemctl stop ' + z)
+    print(y + ' stopped sucessfully!' if process_output[0] == 0 else 'Failed!')
+
 elif 'drop cash' in cmd :
     process_output = spb.getoutput('sudo echo 3 > /proc/sys/vm/drop_caches')
 
 elif 'df -h' in cmd or 'memory information' in cmd : 
      process_output = spb.getoutput('sudo df -h')
-
-elif 'systemctl status httpd' in cmd or 'server status' in cmd:
-     process_output = spb.getoutput('sudo systemctl status httpd')
-
-elif 'systemctl status docker' in cmd or 'docker status' in cmd :
-     process_output = spb.getoutput('sudo systemctl status docker')
 
 elif 'ps -aux' in cmd or 'running processes' in cmd : 
      process_output = spb.getoutput('sudo ps -aux')
@@ -44,4 +52,28 @@ elif 'netstat -tnlp' in cmd  or 'ports' in cmd :
 elif 'ifconfig enp0s3' in cmd or 'ip configuration' in cmd :
     process_output = spb.getoutput('sudo ifconfig enp0s3')
 
+elif 'fdisk -l' in cmd or 'show hard disk' in cmd:
+    process_output = spb.getoutput('sudo fdisk -l')
+
+elif 'route -n' in cmd or 'show route table' in cmd:
+    process_output = spb.getoutput('sudo route -n')
+
+elif 'pvdisplay' in cmd or 'show physical volume' in cmd:
+    process_output = spb.getoutput('sudo pvdisplay')
+
+elif 'vgdisplay' in cmd or 'show volume group' in cmd : 
+    process_output = spb.getoutput('sudo vgdisplay')
+
+elif 'lvdisplay' in cmd or 'show logical volume' in cmd:
+    process_output = spb.getoutput('sudo lvdisplay')
+
+elif 'lsblk' in cmd or 'show partitions' in cmd:
+    process_output = spb.getoutput('sudo lsblk')
+
+elif 'history' in cmd or 'show history' in cmd :
+    process_output = spb.getoutput('history')
+
+
+
 print(process_output)
+
