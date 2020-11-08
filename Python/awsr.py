b6bbc9f85217ac7a4228f008996084807ff6e381
Yash Indane <yashindane46@gmail.com>
2020-11-08 09:03:40 +0530

    Add files via upload

---
+++ Python/awsr.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+
+import cgi
+from subprocess import getstatusoutput as gso
+
+print('content-type:text/plain')
+print()
+
+form_values = cgi.FieldStorage()
+mode = form_values.getvalue('i0')
+
+if mode == 'start instance' : 
+
+   id = form_values.getvalue('i1')
+   status = gso(f'sudo /usr/local/bin/aws ec2 start-instances --instance-ids {id}')
+   print('ec2 instance started!' if status[0]==0 else 'Failed to start!')
+
+elif mode == 'stop instance' :
+  
+   id = form_values.getvalue('i1')
+   status = gso(f'sudo /usr/local/bin/aws ec2 stop-instances --instance-ids {id}')
+   print('ec2 instance stopping!' if status[0]==0 else 'Failed to stop!')
+
+elif mode == 'create SG':
+   
+   group_name = '"'+form_values.getvalue('i1')+'"'
+   group_des =  '"'+form_values.getvalue('i2')+'"'
+
+   status = gso(f'sudo /usr/local/bin/aws ec2 create-security-group --description {group_des} --group-name {group_name}')
+ 
+   print('security group created!' if status[0]==0 else 'Failed to create SG!')
+
+elif mode == 'create S3' : 
+
+
+   bucket_name = form_values.getvalue('i1')
+   region = form_values.getvalue('i2')
+
+   status = gso(f'sudo /usr/local/bin/aws s3api create-bucket --bucket {bucket_name} --region {region} --create-bucket-configuration LocationConstraint={region}')
+   print('Bucket created!' if status[0]==0 else 'Failed to create bucket!')   
+
+elif mode == 'upload to bucket' : 
+
+   bucket_name = form_values.getvalue('i1')
+   path = form_values.getvalue('i2')
+   status = gso(f'sudo /usr/local/bin/aws s3 cp {path} s3://{bucket_name}/')
+   print('file uploaded to bucket!' if status[0]==0 else 'Failed to upload!')
+
+elif mode == 'create cdn' : 
+    
+   origin = form_values.getvalue('i1')
+   status = gso(f'sudo /usr/local/bin/aws cloudfront create-distribution --origin-domain-name {origin}')
+   print('Distribution created!' if status[0]==0 else 'Failed to create distribution')
+
+
