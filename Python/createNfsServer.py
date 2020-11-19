8576eb71c5785a43631f0184a2dab747239d278f
Yash Indane <yashindane46@gmail.com>
2020-11-19 23:42:09 +0530

    Add files via upload

---
+++ Python/createNfsServer.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+
+import cgi
+from subprocess import getstatusoutput as spb
+
+print('content-type:text/html')
+print()
+
+def createServer(X):
+
+ remoteIP , targetIP , file , permi , ss = X
+
+ status = spb('sudo cp /etc/exports /ctemp')
+ status = spb('sudo chown apache /ctemp/exports')
+
+ ANS = 'sudo /usr/local/bin/ansible all -m' 
+
+ status = spb(f'{ANS} command -a"mkdir /{file}"')
+ 
+ if permi == 'Read-Write' : 
+    A = f'"/{file} {targetIP} (rw,no_root_squash)"'
+
+ else :
+    A = f'"/{file} {targetIP} (ro)"'
+ 
+
+ 
+ status = spb(f'sudo echo {A} >> /ctemp/exports')
+ status = spb(f'{ANS} copy -a"src=/ctemp/exports dest=/etc"')  
+
+ print('NFS-server successfully configured!' if status[0]==0 else 'Failed to configure!')
+
+ if ss == 'Yes' : 
+    status = spb(f'{ANS} command -a"systemctl start nfs-server"')
+    print('NFS-server started!' if status[0]==0 else 'Failed to start!')
+ 
+ 
+
+form_values = cgi.FieldStorage()
+
+remoteIP = form_values.getvalue('rip')
+targetIP = form_values.getvalue('gip')
+file = form_values.getvalue('ffn')
+permi = form_values.getvalue('per')
+ss = form_values.getvalue('st')
+
+createServer([remoteIP , targetIP , file , permi , ss])
