06b3e1444d79546c69b23f1e6538c53f4d6e8a4c
Yash Indane <yashindane46@gmail.com>
2020-12-29 11:54:16 +0530

    Update aww.js

--- JS/aww.js
+++ JS/aww.js
@@ -1,6 +1,6 @@
 function stIns(){
 
- document.write("<form id = 'f' action = http://IP/cgi-bin/awsr.py>");
+ document.write("<form id = 'f' action = 'http://IP/cgi-bin/awsr.py'>");
  document.write("<input type = 'text' value = 'start instance' name = 'i0'></br>")
  document.write("<input type = 'text' placeholder = 'Enter instance ID' name = 'i1'></br>");
  document.write("<input type = 'submit' value = 'Start'>");
@@ -11,8 +11,9 @@ function stIns(){
 
 function spIns(){
 
- document.write("<form id = 'f' action = http://IP/cgi-bin/awsr.py>");
+ document.write("<form id = 'f' action = 'http://IP/cgi-bin/awsr.py'>");
  document.write("<input type = 'text' value = 'stop instance' name = 'i0'></br>")
+
  document.write("<input type = 'text' placeholder = 'Enter instance ID' name = 'i1'></br>")
  document.write("<input type = 'submit' value = 'Stop' >");
  document.write('</form>');
@@ -22,8 +23,9 @@ function spIns(){
 
 function crSg(){
 
- document.write("<form id = 'f' action = http://IP/cgi-bin/awsr.py");
+ document.write("<form id = 'f' action = 'http://IP/cgi-bin/awsr.py'>");
  document.write("<input type = 'text' value = 'create SG' name = 'i0'></br>")
+
  document.write("<input type = 'text' placeholder = 'Enter group name' name = 'i1'></br>");
  document.write("<input type 'text' placeholder = 'Enter description' name = 'i2'></br>")
  document.write("<input type = 'submit' value = 'Create' >");
@@ -34,8 +36,9 @@ function crSg(){
 
 function crS3(){
 
- document.write("<form id = 'f' action = http://IP/cgi-bin/awsr.py>");
+ document.write("<form id = 'f' action = 'http://IP/cgi-bin/awsr.py'>");
  document.write("<input type = 'text' value = 'create S3' name = 'i0'></br>")
+
  document.write("<input type = 'text' placeholder = 'Enter bucket name' name = 'i1'></br>");
  document.write("<input type 'text' placeholder = 'Enter region' name = 'i2'></br>");
  document.write("<input type = 'submit' value = 'Create' >");
@@ -46,8 +49,9 @@ function crS3(){
 
 function upS3(){
 
- document.write("<form id = 'f' action = http://IP/cgi-bin/awsr.py>");
+ document.write("<form id = 'f' action = 'http://IP/cgi-bin/awsr.py'>");
  document.write("<input type = 'text' value = 'upload to bucket' name = 'i0'></br>")
+
  document.write("<input type = 'text' placeholder = 'Enter bucket name' name = 'i1'></br>");
  document.write("<input type 'text' placeholder = 'Enter file path' name = 'i2'></br>")
  document.write("<input type = 'submit' value = 'Upload' >");
@@ -58,13 +62,29 @@ function upS3(){
 
 function crDs(){
 
- document.write("<form id = 'f' action = http://IP/cgi-bin/awsr.py>");
+ document.write("<form id = 'f' action = 'http://IP/cgi-bin/awsr.py'>");
  document.write("<input type = 'text' value = 'create cdn' name = 'i0'></br>")
+
  document.write("<input type = 'text' placeholder = 'Enter origin name' name = 'i1'></br>");
  document.write("<input type = 'submit' value = 'Create' >");
  document.write('</form>');
  document.write("");
- 
+
 }
+
+function lb(){
+
+ document.write("<form id = 'f' action = 'http://IP/cgi-bin/createLB.py'>");
+ document.write("<input type = 'text' placeholder = 'Enter load-balancer name' name = 'lbn'></br>");
+ document.write("<input type = 'text' placeholder = 'Enter backend-server name' name = 'bes1'></br>");
+ document.write("<input type = 'text' placeholder = 'Enter backend-server name' name = 'bes2'></br>");
+ document.write("<input type = 'submit' value = 'Create'>");
+ document.write("</form>");
+ document.write("");
+
+}
+
+
+
 
 
