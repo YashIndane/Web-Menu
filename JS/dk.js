6f18ad808f78d31192de105c26b897279ee7d878
Yash Indane <yashindane46@gmail.com>
2021-01-26 23:28:36 +0530

    Update dk.js

--- JS/dk.js
+++ JS/dk.js
@@ -1,53 +1,57 @@
 const button = document.getElementById('sb');
-const osname = document.getElementById('d_')
+const osname = document.getElementById('d_');
 const version = document.getElementById('v_');
 const na = document.getElementById('n_');
-const submitbutton = document.getElementById('ss');
+
+var counter = 0;
+const fields = [osname , version , na];
+
+const submitbutton = document.getElementById('sub');
 
 const SR = window.SpeechRecognition || window.webkitSpeechRecognition;
 const recog = new SR();
-var counter = 0;
-const fields = [osname , version , na];
+
 
 recog.onstart = () => console.log('speak');
 
 recog.onresult = function(event){
+
                  const current = event.resultIndex;
                  const transcript = event.results[current][0].transcript;
-                 
                  fields[counter].value = transcript;
-
-                 
-                 //textfield.value = transcript;
-                 //submitbutton.click();
+                 counter += 1;
+                 submitbutton.click();
 };
 
-
 function speak(msg){
-        
-       const sph = new SpeechSynthesisUtterance();
-       sph.text = msg;
-       sph.volume = 1;
-       sph.rate = 1;
-       sph.pitch = 1;
-       window.speechSynthesis.speak(sph);
+
+    const speech = new SpeechSynthesisUtterance();
+    speech.text = msg;
+    speech.volume = 1;
+    speech.rate = 1;
+    speech.pitch = 1;
+    window.speechSynthesis.speak(speech);
 }
 
-
-
-function getDetails(){
-
-       speak("Please tell your OS");
-       recog.start();
-       counter += 1;
-       speak("Please tell your version");
-       recog.start();
-       counter += 1;
-       speak("What name you would like to give to the OS?");
-       recog.start();
-       speak("Launching the OS")
-       submitbutton.click();
-
+const DEF_DELAY = 1000;
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms || DEF_DELAY));
 }
 
-button.onclick = () => getDetails();
+//await sleep(100);
+
+(async()=>{
+
+   speak('Please tell your container image name');
+   await sleep(4500);
+   recog.start();
+   await sleep(4500);
+   speak('Please tell your image version');
+   await sleep(4500);
+   recog.start();
+   await sleep(4500);
+   speak('Please tell your container name');
+   await sleep(4500);
+   recog.start();
+
+})()
