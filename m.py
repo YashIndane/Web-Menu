c3149113860382f6f0319c6e12d058effcf9c34d
Yash Indane <yashindane46@gmail.com>
2021-02-08 21:32:44 +0530

    Update m.py

--- m.py
+++ m.py
@@ -9,4 +9,11 @@ print()
 form_values = cgi.FieldStorage()
 cmd = form_values.getvalue("v")
 
-print(cmd)
+cmd = cmd.lower()
+T = False
+
+if 'free ram' in cmd : 
+    process_output = spb.getoutput('free -m')
+
+elif 'date' in cmd : 
+    process_output = spb.getoutput('date')
