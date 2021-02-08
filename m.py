44ba5a4e7f778c5d10be31315c31b52f418700d9
Yash Indane <yashindane46@gmail.com>
2021-02-08 21:38:32 +0530

    Update m.py

--- m.py
+++ m.py
@@ -17,3 +17,67 @@ if 'free ram' in cmd :
 
 elif 'date' in cmd : 
     process_output = spb.getoutput('date')
+
+elif 'calendar' in cmd : 
+    process_output = spb.getoutput('cal')
+
+elif 'cpu details' in cmd : 
+    process_output = spb.getoutput('sudo lscpu')
+
+elif 'status of' in cmd :
+    x = cmd.split(' ')[-1]
+    process_output = spb.getoutput('sudo systemctl status ' + x)
+
+elif 'start' in cmd : 
+    y = cmd.split(' ')[-1]
+    process_output = spb.getstatusoutput('sudo systemctl start ' + y)
+    print(y + ' started sucessfully!' if process_output[0] == 0 else 'Failed!')
+
+elif 'stop' in cmd : 
+    z = cmd.split(' ')[-1]
+    process_output = spb.getstatusoutput('sudo systemctl stop ' + z)
+    print(z + ' stopped sucessfully!' if process_output[0] == 0 else 'Failed!')
+
+elif 'drop cash' in cmd :
+    process_output = spb.getoutput('sudo echo 3 > /proc/sys/vm/drop_caches')
+
+elif 'memory information' in cmd : 
+     process_output = spb.getoutput('sudo df -h')
+
+elif 'running processes' in cmd : 
+     process_output = spb.getoutput('sudo ps -aux')
+
+elif 'ports' in cmd : 
+     process_output = spb.getoutput('sudo netstat -tnlp')
+
+elif 'ip configuration' in cmd :
+    process_output = spb.getoutput('sudo ifconfig enp0s3')
+
+elif 'show hard disk' in cmd:
+    process_output = spb.getoutput('sudo fdisk -l')
+
+elif 'show route table' in cmd:
+    process_output = spb.getoutput('sudo route -n')
+
+elif 'show physical volume' in cmd:
+    process_output = spb.getoutput('sudo pvdisplay')
+
+elif 'show volume group' in cmd : 
+    process_output = spb.getoutput('sudo vgdisplay')
+
+elif 'show logical volume' in cmd:
+    process_output = spb.getoutput('sudo lvdisplay')
+
+elif 'show partitions' in cmd:
+    process_output = spb.getoutput('sudo lsblk')
+
+elif 'show history' in cmd :
+    process_output = spb.getoutput('history')
+
+elif 'routing table' in cmd:
+    process_output = spb.getoutput('sudo route -n')
+
+else : 
+   process_output = spb.getstatusoutput('sudo ' + cmd)
+   print("Successful" if process_output[0]==0 else "Failed")
+   T = True
