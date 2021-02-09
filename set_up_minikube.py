c595dfd96991224c9c55279900066dbc0331c565
Yash Indane <yashindane46@gmail.com>
2021-02-09 19:26:58 +0530

    Update set_up_minikube.py

--- set_up_minikube.py
+++ set_up_minikube.py
@@ -11,10 +11,10 @@ form_values = cgi.FieldStorage()
 remote_ip = form_values.getvalue('-ip-')
 passwd = form_values.getvalue('pw')
 
-gso('sudo chown apache /inven.txt')
+status = gso('sudo chown apache /inven.txt')
 
-gso('sudo /usr/bin/echo "[minikube_ip]" >> /inven.txt')
-gso(f'sudo /usr/bin/echo "{remote_ip} ansible_user=root ansible_ssh_pass={passwd} ansible_connection=ssh" >> /inven.txt')
+status = gso('sudo /usr/bin/echo "[minikube_ip]" >> /inven.txt')
+status = gso(f'sudo /usr/bin/echo "{remote_ip} ansible_user=root ansible_ssh_pass={passwd} ansible_connection=ssh" >> /inven.txt')
 status = gso('sudo /usr/local/bin/ansible-playbook /ansible-scripts/mini.yml')
 
 print('successful' if status[0]==0 else 'failed')
