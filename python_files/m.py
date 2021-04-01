f43fac9b01fcf66c697fc80c4f8602b5c33ca07c
Yash Indane <yashindane46@gmail.com>
2021-04-01 15:17:12 +0530

    Update m.py

--- python_files/m.py
+++ python_files/m.py
@@ -13,20 +13,23 @@ cmd = cmd.lower()
 T = False
 
 if 'free ram' in cmd : 
-    process_output = spb.getoutput('free -m')
+    process_output = spb.getstatusoutput('free -m')
 
 elif 'date' in cmd : 
-    process_output = spb.getoutput('date')
+    process_output = spb.getstatusoutput('date')
 
 elif 'calendar' in cmd : 
-    process_output = spb.getoutput('cal')
+    process_output = spb.getstatusoutput('cal')
 
 elif 'cpu details' in cmd : 
-    process_output = spb.getoutput('sudo lscpu')
+    process_output = spb.getstatusoutput('sudo lscpu')
+    
+elif 'uptime' in cmd :
+    process_output = spb.getstatusoutput('sudo uptime')
 
 elif 'status of' in cmd :
     x = cmd.split(' ')[-1]
-    process_output = spb.getoutput('sudo systemctl status ' + x)
+    process_output = spb.getstatusoutput('sudo systemctl status ' + x)
 
 elif 'start' in cmd : 
     y = cmd.split(' ')[-1]
@@ -36,52 +39,50 @@ elif 'start' in cmd :
 elif 'stop' in cmd : 
     z = cmd.split(' ')[-1]
     process_output = spb.getstatusoutput('sudo systemctl stop ' + z)
-    print(y + ' stopped sucessfully!' if process_output[0] == 0 else 'Failed!')
+    print(z + ' stopped sucessfully!' if process_output[0] == 0 else 'Failed!')
 
 elif 'drop cash' in cmd :
-    process_output = spb.getoutput('sudo echo 3 > /proc/sys/vm/drop_caches')
+    process_output = spb.getstatusoutput('sudo echo 3 > /proc/sys/vm/drop_caches')
 
 elif 'memory information' in cmd : 
-     process_output = spb.getoutput('sudo df -h')
+     process_output = spb.getstatusoutput('sudo df -h')
 
 elif 'running processes' in cmd : 
-     process_output = spb.getoutput('sudo ps -aux')
+     process_output = spb.getstatusoutput('sudo ps -aux')
 
 elif 'ports' in cmd : 
-     process_output = spb.getoutput('sudo netstat -tnlp')
+     process_output = spb.getstatusoutput('sudo netstat -tnlp')
 
 elif 'ip configuration' in cmd :
-    process_output = spb.getoutput('sudo ifconfig enp0s3')
+    process_output = spb.getstatusoutput('sudo ifconfig enp0s3')
 
 elif 'show hard disk' in cmd:
-    process_output = spb.getoutput('sudo fdisk -l')
+    process_output = spb.getstatusoutput('sudo fdisk -l')
 
 elif 'show route table' in cmd:
-    process_output = spb.getoutput('sudo route -n')
+    process_output = spb.getstatusoutput('sudo route -n')
 
 elif 'show physical volume' in cmd:
-    process_output = spb.getoutput('sudo pvdisplay')
+    process_output = spb.getstatusoutput('sudo pvdisplay')
 
 elif 'show volume group' in cmd : 
-    process_output = spb.getoutput('sudo vgdisplay')
+    process_output = spb.getstatusoutput('sudo vgdisplay')
 
 elif 'show logical volume' in cmd:
-    process_output = spb.getoutput('sudo lvdisplay')
+    process_output = spb.getstatusoutput('sudo lvdisplay')
 
 elif 'show partitions' in cmd:
-    process_output = spb.getoutput('sudo lsblk')
+    process_output = spb.getstatusoutput('sudo lsblk')
 
 elif 'show history' in cmd :
-    process_output = spb.getoutput('history')
+    process_output = spb.getstatusoutput('history')
 
 elif 'routing table' in cmd:
-    process_output = spb.getoutput('sudo route -n')
+    process_output = spb.getstatusoutput('sudo route -n')
 
 else : 
    process_output = spb.getstatusoutput('sudo ' + cmd)
    print("Successful" if process_output[0]==0 else "Failed")
    T = True
-
-
 
 print(process_output[1] if T else process_output)
